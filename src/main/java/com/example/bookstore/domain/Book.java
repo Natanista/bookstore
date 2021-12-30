9badd31974828336edf12a04dfdf2048254e8e22
natanista <natan.silva@bandtec.com.br>
2021-12-29 22:16:17 -0300

    Adding domain classes

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,73 @@
+package com.example.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Integer id;
+    private String title;
+    private String author;
+    private String content;
+    private Category category;
+
+    public Book(Integer id, String title, String author, String content, Category category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.content = content;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
